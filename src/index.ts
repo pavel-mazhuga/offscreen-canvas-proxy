239731c7ec750e79d39d8960cacdbddaab2cbcd9
pavel-mazhuga <pavelmazhuga@yandex.ru>
2020-03-09 19:17:50 +0700

    offscreen-canvas

--- src/index.ts
+++ src/index.ts
@@ -3,32 +3,45 @@ import { wrap } from 'comlink';
 interface ProxyData {
     canvas: HTMLCanvasElement;
     workerUrl: string;
-    name: string;
+    id: string;
 }
+
+export interface OffscreenBaseData {
+    canvas: HTMLCanvasElement;
+    isWorker: boolean;
+}
+
 /**
  * @param  {} canvas - HTMLCanvasElement
  * @param  {} workerUrl - path to the worker file
- * @param  {} name - unique name for accessing module from 'window' object
+ * @param  {} id - unique id for accessing module from 'window' object
  * (if 'transferControlToOffscreen' is not supported)
  */
-export default ({ canvas, workerUrl, name }: ProxyData, data: unknown[]) => {
+export function createOffscreenCanvas<T>({ canvas, workerUrl, id }: ProxyData, data: Record<string, any>): Promise<T> {
     return new Promise((resolve, reject) => {
         if (canvas.transferControlToOffscreen) {
             try {
                 const worker = new Worker(workerUrl);
                 const offscreen = canvas.transferControlToOffscreen();
-                const proxy = wrap(worker);
+
+                worker.addEventListener('message', event => {
+                    if (event.data === 'ready') {
+                        const proxy = wrap<T>(worker);
+                        resolve(proxy);
+                    }
+                });
 
                 worker.postMessage(
                     {
-                        canvas: offscreen,
-                        isWorker: true,
-                        ...data,
+                        message: 'init',
+                        options: {
+                            canvas: offscreen,
+                            isWorker: true,
+                            ...data,
+                        },
                     },
                     [offscreen],
                 );
-
-                resolve(proxy);
             } catch (err) {
                 reject(err);
             }
@@ -38,17 +51,39 @@ export default ({ canvas, workerUrl, name }: ProxyData, data: unknown[]) => {
             script.async = true;
             script.onload = () => {
                 resolve(
-                    (window as any)[name]({
+                    new (window as any)[id]({
                         canvas,
                         isWorker: false,
                         ...data,
                     }),
                 );
-                (window as any)[name] = null;
+                (window as any)[id] = null;
             };
-
             script.onerror = err => reject(err);
             document.head.appendChild(script);
         }
     });
-};
+}
+
+export class BaseEntity {
+    private rAF: number;
+
+    canvas: HTMLCanvasElement;
+
+    isWorker: boolean;
+
+    constructor(options: OffscreenBaseData) {
+        this.rAF = 0;
+        this.state = {};
+        this.canvas = options.canvas;
+        this.isWorker = options.isWorker;
+    }
+
+    async getState() {
+        return this.state;
+    }
+
+    async setState(newState = {}) {
+        this.state = { ...this.state, ...newState };
+    }
+}
